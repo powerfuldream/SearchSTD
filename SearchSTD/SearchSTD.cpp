4feac4bbed94ecf03f76b58d3e10ef8f4c367586
line <line@DESKTOP-KO3QS7K>
2019-01-14 18:11:47 -0800

    f

--- SearchSTD/SearchSTD.cpp
+++ SearchSTD/SearchSTD.cpp
@@ -3,9 +3,25 @@
 
 #include "stdafx.h"
 
+int BinarySearch(int *array, int key, int low, int high);
 
 int main()
 {
     return 0;
 }
 
+int BinarySearch(int * arr, int key, int low, int high)
+{
+	int mid;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		if (key == arr[mid])
+			return mid;
+		else if (key < arr[mid])
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return 0;
+}
